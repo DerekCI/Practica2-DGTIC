b6c85cee2056f15a8a78c0c67954aed47f1daccf
Derek Cortez <derekcortez16@gmail.com>
2022-11-01 15:00:54 -0600

    Interfaz agregada

--- app/src/main/java/com/derekci/practica2/MainActivity.kt
+++ app/src/main/java/com/derekci/practica2/MainActivity.kt
@@ -2,10 +2,36 @@ package com.derekci.practica2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.derekci.practica2.databinding.ActivityMainBinding
 
+lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val spinner = binding.spinner
+        val opciones = resources.getStringArray(R.array.opciones)
+
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, opciones)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                ) {
+
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
+        }
     }
 }
